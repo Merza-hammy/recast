34ebfcfbd050f3248c50b78852da51e46cf29eeb
hammy <44582470+Merza-hammy@users.noreply.github.com>
2018-12-28 13:03:16 -0600

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,13 +1,26 @@
-<?php
-
-require('../vendor/autoload.php');
+<?php 
+require 'vendor/autoload.php';
 
 	include_once "../webhook/somosioticos_dialogflow.php";
 	credenciales('hammy','123456789');
 	
-	
-		
-	
+use \Slim\App; 
+ 
+$app = new App(); 
+ 
+$app->post('/', function ($request, $response) { 
+  error_log($request->getBody()->getContents()); 
+  return $response->withJson(array( 
+    'replies' => [ 
+      array('type' => 'text', 'content' => 'Roger that') 
+    ], 
+    'conversation' => array( 
+      'memory' => array('key' => 'value') 
+    ) 
+  )); 
+}); 
+
+
 	if (intent_recibido("calcular")){
 	
 		$valor1 = obtener_variables()['numero1'];
@@ -16,11 +29,20 @@ require('../vendor/autoload.php');
 		enviar_texto("el resultado es $resultado");
 	}
 	
-	
+ 
+$app->post('/errors', function ($request, $response) { 
+  error_log($request->getBody()->getContents()); 
+  return $response; 
+}); 
+ 
+$app->run();
+
+
+
+
 
 
 	
-   
-
-
-?>
+		
+	
+	
